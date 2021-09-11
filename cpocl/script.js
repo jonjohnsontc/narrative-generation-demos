668a2dd569ff9483ec224861cccbfaa478d3651e
Jon Johnson <jonjohnsontc@live.com>
2021-09-11 16:46:28 -0700

    throwing poop at the wall

--- cpocl/script.js
+++ cpocl/script.js
@@ -2,23 +2,28 @@
 // A program which, given a state, character goals, and author's goal,
 // runs through one step at a time to generate a narrative. Each step
 // should be printed to the console, or displayed in the browser
-/**
- * Description
- * @param {any} expr
- * @returns {any} An intentionPlan with expr as the goal
- */
-function intendsTo(expr) {
-  let intentionFrame;
-  return intentionFrame
+
+function intendsTo(char, goal, motivatingStep, satisfyingStep) {
+  return new IntentionFrame(char, goal, motivatingStep, satisfyingStep, []);
+}
+
+class IntentionFrame {
+  constructor(char, goal, motivatingStep, satisfyingStep, subPlan) {
+    this.char = char,
+    this.goal = goal,
+    this.motivatingStep = motivatingStep
+    this.satisfyingStep = satisfyingStep
+    this.subPlan = subPlan
+  }
 }
 
 /** Denotes a step in a CPOCL Plan*/
 class Step {
   /**
    * Creates a step
-   * @param {Array} preconditions
-   * @param {Map} effects
-   * @param {boolean} persistant
+   * @param {Array<Function>} preconditions
+   * @param {Function} effects Function to apply to modify state
+   * @param {Boolean} persistant Whether step should be executed every turn
    * @param {String} description description of the Step. It will be printed to the screen if executed.
    */
   constructor(description, persistant, preconditions, effects) {
@@ -27,7 +32,26 @@ class Step {
     this.preconditions = preconditions;
     this.effects = effects;
   }
+
   executed = false;
+
+  /**
+   * Executes event and returns a copy of the state with changes
+   * @param {Map<symbol, Map<symbol, Array<String> | String | boolean>>} state
+   * @returns {Map<symbol, Map<symbol, Array<String> | String | boolean>>} new state
+   */
+  execute(state) {
+    if (this.preconditions.every((x) => x(state) == true)) {
+      let newState = Object.assign({}, state);
+      this.effects(newState);
+      console.log(this.description);
+      this.executed = true;
+      return newState;
+    } else {
+      console.log("all preconditions not met");
+      return state;
+    }
+  }
 }
 
 // Here I am setting up 'domain' functions pursuant to the CPOCL example
@@ -44,43 +68,62 @@ const travel = (char, from, to) => {
 
 class Travel extends Step {
   constructor(preconditions, char, from, to) {
-    this.char = char;
-    this.from = from;
-    this.to = to;
-
     // TODO: How is the planner supposed to know what this does?
     // Perhaps if it knows what `this.to` is ahead of time
-    this.effects = (p) => (p.at = to);
-    let defaultPreconditions = [char.health > 0, char.at == from, from !== to];
-    let allPreconditions = preconditions.concat(defaultPreconditions);
+    const effects = (state) => (state[char].at = to);
+    const defaultPreconditions = [
+      (state) => state[char].alive == true,
+      (state) => state[char].at == from,
+      () => from !== to,
+    ];
+    const allPreconditions = preconditions.concat(defaultPreconditions);
     super(
       `${char} travels from ${from} to ${to}`,
       false,
       allPreconditions,
       effects
     );
-  }
-  execute(state) {
-    if (this.preconditions.every(x => !!x)) {
-      let newState = Object.assign({}, state);
-      
-      this.effects(newState.Timmy)
-      
-      console.log(this.description);
-      this.executed = true;
-      return newState;
-    }
+    // TODO: Figure out if you need these properties around
+    this.char = char;
+    this.from = from;
+    this.to = to;
   }
 }
 
 class Snakebite extends Step {
-  constructor()
-  super()
+  constructor(preconditions, char) {
+    const defaultPreconditions = [(state) => state[char].alive === true];
+    const allPreconditions = preconditions.concat(defaultPreconditions);
+    const effects = (state) => {
+      intendsTo(char, state[char].status == "dying", this, null);
+      super(`${char} is bitten by snake`, false, allPreconditions, effects);
+    };
+  }
+}
 
-  effects = p => {
-
-    // Intends to needs to lead to an intention frame being spawned
-    intendsTo(p.alive === false)
+class Take extends Step {
+  constructor(preconditions, item, takeChar, fromChar) {
+    // the fromChar actually needs the item
+    // the takeChar needs to be alive
+    // both Chars need to be in the same location
+    const defaultPreconditions = [
+      (state) => state[fromChar].inventory.includes(item),
+      (state) => state[takeChar].alive === true,
+      (state) => state[fromChar].at === state[takeChar].at,
+    ];
+    const allPreconditions = preconditions.concat(defaultPreconditions);
+    const effects = (state) => {
+      state[fromChar].inventory = state[fromChar].inventory.filter(
+        (i) => i !== item
+      );
+      state[takeChar].inventory.push(item);
+    };
+    super(
+      `${takeChar} takes ${item} from ${fromChar}`,
+      false,
+      allPreconditions,
+      effects
+    );
   }
 }
 
@@ -93,7 +136,7 @@ const state = {
     armed: true,
     sheriff: false,
     at: "Ranch",
-    inventory: {},
+    inventory: [],
     status: "healthy",
   },
   Timmy: {
@@ -103,7 +146,7 @@ const state = {
     armed: false,
     sheriff: false,
     at: "Ranch",
-    inventory: {},
+    inventory: [],
     status: "healthy",
   },
   William: {
@@ -113,7 +156,7 @@ const state = {
     armed: true,
     sheriff: true,
     at: "Saloon",
-    inventory: {},
+    inventory: [],
     status: "healthy",
   },
   Carl: {
@@ -123,12 +166,28 @@ const state = {
     armed: true,
     sheriff: false,
     at: "Saloon",
-    inventory: {},
+    inventory: [],
     status: "healthy",
   },
 };
 
-const endState = state.Timmy.alive == false || state.Timmy.saved == true;
+// I'm not yet sure whether it's best to represent the end state as a function,
+// or as a Map, with which I can compare by copying the current state and adding
+// in the endState
+// Actually, I don't know if just "adding in the endState" is possible with a map
+const isEndState = (state) => state.Timmy.alive == false || state.Timmy.saved == true;
+
+const eState1 = {
+  Timmy: {
+    status: "dying",
+  },
+};
+
+const eState2 = {
+  Timmy: {
+    status: "-dying",
+  },
+};
 
 /**
  * Returns the index of a step if it exists
@@ -201,20 +260,71 @@ function calculateDuration(m1, m2, t, u, if1, if2) {
   let start = Math.max();
 }
 
+const coinFlip = () => {
+  return Math.floor(Math.random() * 2);
+};
+
+const randIdx = (arr) => {
+  return Math.floor(Math.random() * arr.length);
+};
+
 class CPOCL {
-  state = state;
-  history = [state];
-  desiredState = this.state.Timmy.alive == false || this.state.Timmy.status == "saved";
+  constructor(startState, endState) {
+    this.history = [];
+    this.flaws = [];
+    this.state = startState;
+    this.operators = [];
+    this.endState = new Function(...endState)
+  }
 
   cpocl(steps, varBindings, stepOrdering, causalLinks, intentionFrames) {
+    flaw = this.getFlaw();
+    
+    switch (flaw.type) {
+      case "open precondition flaw":
+        let sAdd = this.getStep(steps);
+        break;
+    }
+    
+
     let newState = undefined;
     return newState;
   }
 
+  createStep() {
+    let newStep;
+    return newStep;
+  }
+
+  /**
+   * Returns a random flaw from the list of flaws
+   * @returns {any} A random Flaw object
+   */
+  getFlaw() {
+    idx = randIdx(flaws);
+    flaw = this.flaws[idx];
+    this.flaws.splice(idx, 1);
+    return flaw;
+  }
+
+  getStep(steps) {
+    if (steps.length > 0) {
+      answer = coinFlip();
+      if (answer == 1) {
+        idx = randIdx(steps);
+        return steps[idx];
+      } else {
+        return this.createStep();
+      }
+    } else {
+      // create new step
+      return this.createStep();
+    }
+  }
+
   step() {
     if (this.desiredState == true) {
-      return "Yay you won"
+      return "Fin";
     }
-    let flaws =  
   }
 }
